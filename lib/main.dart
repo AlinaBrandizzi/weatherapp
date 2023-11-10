2b68254209df38865f25b0aa0e03df353b1b56ac
AlinaBrandizzi <alinabrandizzi@gmail.com>
2023-11-10 16:25:48 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'card.dart';
+=======
+>>>>>>> Stashed changes
 
 void main() {
   runApp(const MyApp());
@@ -13,11 +16,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+<<<<<<< Updated upstream
       home: HomePage(),
+=======
+      home:
+          FakeMain(), //Fake Main perché purtroppo continuo ad avere errori di compiling ad ogni debug, questa è la mia UI placeholder
+>>>>>>> Stashed changes
     );
   }
 }
 
+<<<<<<< Updated upstream
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -250,6 +259,83 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
+=======
+class FakeMain extends StatelessWidget {
+  const FakeMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 187, 60, 96),
+        body: Center(
+          child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(30, 0, 30, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+                  child: Text(
+                    'Roma',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.w800),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: FittedBox(
+                    child: Text('Ciao, dopo inserisco il bottone data!'),
+                  ),
+                ),
+                Text(
+                  '23°',
+                  style: TextStyle(fontSize: 200, fontWeight: FontWeight.w400),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.sunny,
+                      color: Colors.black,
+                      size: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(10, 5, 5, 5),
+                      child: Text(
+                        'Soleggiato',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                  child: Text('Ciao, dopo inserisco il banner!'),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                  child: Text(
+                    'Previsioni orarie',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                (Text('Ciao, dopo inserisco i bottoncini!')),
+              ],
+            ),
+          ),
+        ),
+      )),
+>>>>>>> Stashed changes
     );
   }
 }
